e77f1a89987d5f348a64f46535aa54ec27607122
Carlos12001 <carlos.andres12001@gmail.com>
2023-05-05 13:11:42 -0600

    fix: repair value variable max win

--- src/check_win.sv
+++ src/check_win.sv
@@ -1,6 +1,7 @@
-module check_win #(parameter MAX_WIN = 11) (
-  input logic [11:0] board_in[3:0][3:0],
-  output logic result
+module check_win (
+	input logic [11:0]num_max_win,
+	input logic [11:0] board_in[3:0][3:0],
+	output logic result
 );
 
   logic win;
@@ -9,7 +10,7 @@ module check_win #(parameter MAX_WIN = 11) (
     win = 1'b0;
     for (int i = 0; i < 4; i++) begin
       for (int j = 0; j < 4; j++) begin
-        if (board_in[i][j] >= (12'b1 << MAX_WIN)) begin
+        if (board_in[i][j] >= num_max_win) begin
           win = win|1'b1;
         end
       end
