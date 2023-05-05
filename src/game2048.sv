0652440792f4bbe23ba040b5e4d2854a4823b05e
Carlos12001 <carlos.andres12001@gmail.com>
2023-05-05 13:25:50 -0600

    feat: game win is finish

--- src/game2048.sv
+++ src/game2048.sv
@@ -3,7 +3,7 @@
  direction: 0001 top, 0010 bottom, 0100 left, 1000 right
  game_state: 00 not_playing, 01 playing, 10 win, 11 lose
 */
-module game2048 #(parameter MAX_WIN=11) (
+module game2048(
   input logic clk,
   input logic rst,
   input logic [3:0] direction,
@@ -22,12 +22,10 @@ module game2048 #(parameter MAX_WIN=11) (
 	logic move_and_merge_done;
 	logic place_random_start;
 	logic place_random_done;
-	logic check_win_done;
+	logic [11:0] num_max_win = 8;
 	logic check_win_result;
-	logic check_lose_done;
 	logic check_lose_result;
 
-	// Modifica aquí: Agrega la señal move_and_merge_start
 	move_and_merge_tiles move_and_merge (
 	 .direction(direction),
 	 .board_in(board),
@@ -45,16 +43,13 @@ module game2048 #(parameter MAX_WIN=11) (
 	 .done(place_random_done)
 	);
 
-	// check_win win (
-	//   .clk(clk),
-	//   .rst(rst),
-	//   .board_in(board),
-	//   .result(check_win_result)
-	// );
+	check_win win (
+		.num_max_win(num_max_win),
+		.board_in(board),
+		.result(check_win_result)
+	);
 
 	// check_lose lose (
-	//   .clk(clk),
-	//   .rst(rst),
 	//   .board_in(board),
 	//   .result(check_lose_result)
 	// );
@@ -79,10 +74,6 @@ module game2048 #(parameter MAX_WIN=11) (
 			score = 0;
 			game_state = 0;
 			place_random_start = 1;
-			check_win_done = 0;
-			check_win_result = 0;
-			check_lose_done = 0;
-			check_lose_result = 0;
 			next_state = FIRST_RANDOM_TILE;
 		end
 
@@ -133,22 +124,22 @@ module game2048 #(parameter MAX_WIN=11) (
 		end
 
 		CHECK_WIN: begin
-		  // if (check_win_done) begin
-		  //   game_state = 2'b10; // win
-		  //   next_state = GAME_OVER;
-		  // end else begin
-		  //   next_state = CHECK_lose;
-		  // end
-		  next_state = IDLE;
+			if (check_win_result) begin
+				game_state = 2'b10;
+				next_state = GAME_OVER;
+			end else begin
+				next_state = CHECK_LOSE;
+			end
 		end
 
 		CHECK_LOSE: begin
-		  if (check_lose_done) begin
-			 game_state = 2'b11; // lose
-			 next_state = GAME_OVER;
-		  end else begin
-			 next_state = IDLE;
-		  end
+		//   if (check_lose_done) begin
+		// 	 game_state = 2'b11; // lose
+		// 	 next_state = GAME_OVER;
+		//   end else begin
+		// 	 next_state = IDLE;
+		//   end
+		  next_state = IDLE;
 		end
 
 		GAME_OVER: begin
