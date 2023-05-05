54f550cb0dc12745fb6c37850f92f396fc8043b1
Carlos12001 <carlos.andres12001@gmail.com>
2023-05-05 10:33:39 -0600

    chore: rename tie to lose

--- src/game2048.sv
+++ src/game2048.sv
@@ -13,7 +13,7 @@ module game2048 (
 );
 
 	typedef enum logic [3:0] {START, FIRST_RANDOM_TILE, TRANSITION_RANDOM, SECOND_RANDOM_TILE, IDLE, 
-	MOVE_MERGE, NEW_TILE, CHECK_WIN, CHECK_TIE, GAME_OVER} state_t;
+	MOVE_MERGE, NEW_TILE, CHECK_WIN, CHECK_LOSE, GAME_OVER} state_t;
 	state_t current_state, next_state;
 
 	logic [11:0] board_move[3:0][3:0];
@@ -24,8 +24,8 @@ module game2048 (
 	logic place_random_done;
 	logic check_win_done;
 	logic check_win_result;
-	logic check_tie_done;
-	logic check_tie_result;
+	logic check_lose_done;
+	logic check_lose_result;
 
 	// Modifica aquí: Agrega la señal move_and_merge_start
 	move_and_merge_tiles move_and_merge (
@@ -52,11 +52,11 @@ module game2048 (
 	//   .result(check_win_result)
 	// );
 
-	// check_tie tie (
+	// check_lose lose (
 	//   .clk(clk),
 	//   .rst(rst),
 	//   .board_in(board),
-	//   .result(check_tie_result)
+	//   .result(check_lose_result)
 	// );
 
 	always_ff @(posedge clk) begin
@@ -81,8 +81,8 @@ module game2048 (
 			place_random_start = 1;
 			check_win_done = 0;
 			check_win_result = 0;
-			check_tie_done = 0;
-			check_tie_result = 0;
+			check_lose_done = 0;
+			check_lose_result = 0;
 			next_state = FIRST_RANDOM_TILE;
 		end
 
@@ -136,13 +136,13 @@ module game2048 (
 		  //   game_state = 2'b10; // win
 		  //   next_state = GAME_OVER;
 		  // end else begin
-		  //   next_state = CHECK_TIE;
+		  //   next_state = CHECK_lose;
 		  // end
 		  next_state = IDLE;
 		end
 
-		CHECK_TIE: begin
-		  if (check_tie_done) begin
+		CHECK_LOSE: begin
+		  if (check_lose_done) begin
 			 game_state = 2'b11; // lose
 			 next_state = GAME_OVER;
 		  end else begin
